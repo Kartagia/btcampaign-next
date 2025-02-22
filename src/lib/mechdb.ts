16abf90c7eeba064025589a8101669e6a9b2e5b2
Antti Kautiainen <antti@kautiainen.com>
2025-02-22 23:48:21 +0200

    feat: PqMechType Changes to be committed: 	modified:   src/lib/mechdb.ts     - doc: EntryPredicate     - doc: ContentPredicate     - doc: Dao     - doc: Exception     - doc: NotFoundException constructor     - feat: NotFoundException.DefaultMessage     - feat: Identifier parameter for NotFoundException     - rename: InvalidValueException -> InvalidContentException     - doc: InvalidContentException constructor.     - feat: type Status for ts-postgres status message details.     - feat: function parseStatus for parsing a status message.     - feat: class PqMechType for Postgres using Mech database DAO.         - feat: method parseId for parsing stringified identifier to         database identifier.         - feat: method getAll         - feat: method get         - feat: method delete         - feat: method create         - feat: method update         - todo: checking validity of the value.         - todo: checking the identifier with parseId.

--- src/lib/mechdb.ts
+++ src/lib/mechdb.ts
@@ -1,7 +1,11 @@
 
 import { Mech } from "@/data/mechs";
-import { Content } from "next/font/google";
+import type { Unit } from "@/data/mechs";
+import { Content, Rowdies } from "next/font/google";
 import { notFound } from "next/navigation";
+import { stringify } from "querystring";
+import internal from "stream";
+import { Client, Configuration, connect, DataType } from "ts-postgres";
 import { resourceLimits } from "worker_threads";
 
 /**
@@ -16,6 +20,9 @@ import { resourceLimits } from "worker_threads";
 type Predicate<VALUE> = (value: VALUE) => boolean;
 
 
+/**
+ * An entry predicate testing identifier-content pairs.
+ */
 export interface EntryPredicate<CONTENT, ID> {
     /**
      * Test an entry.
@@ -99,7 +106,9 @@ export function createEntryPredicate<CONTENT, ID = string>(predicate: Predicate<
     };
 }
 
-
+/**
+ * The predicate testing a content.
+ */
 export class ContentPredicate<CONTENT, ID> implements EntryPredicate<CONTENT, ID> {
     predicate: Predicate<[ID, CONTENT]>;
 
@@ -128,21 +137,32 @@ export class ContentPredicate<CONTENT, ID> implements EntryPredicate<CONTENT, ID
 
 }
 
-
+/**
+ * Dae represents a database access object.
+ */
 export interface Dao<CONTENT, ID = string> {
 
+    /**
+     * Get all contents.
+     * @param predicate The predicate filtering the entries. Defaults to the predicate 
+     * accepting all values.
+     * @return The promise of the identifier and content pairs fuflillign the predicate.
+     */
     getAll(predicate?: Predicate<[ID, CONTENT]> | EntryPredicate<CONTENT, ID>): Promise<[ID, CONTENT][]>;
 
     /**
-     * 
+     * Get a content with identifier.
      * @param id The identifier of the queried item.
-     * @throws {NotFoundException}
+     * @return The promise of the content with identifier.
+     * @throws {NotFoundException} The rejected error indicating there is no element with the identifier.
      */
     get(id: ID): Promise<CONTENT>;
 
     /**
      * Create a new entry in the dao.
      * @param content THe new content added to the dao.
+     * @return The promise of the identifier assigned to the content.
+     * @throws {InvalidContentException} The rejected error indicating the content was not accepted.
      */
     create(content: CONTENT): Promise<ID>;
 
@@ -152,34 +172,52 @@ export interface Dao<CONTENT, ID = string> {
      * @param newContent The new content.
      * @throws {NotFoundException} The rejected exception indicating there is
      * no content to modify.
+     * @throws {InvalidContentException} The rejected ecxeption indicating the content
+     * was not accepted.
      */
     update(id: ID, newContent: CONTENT): Promise<void>;
 
     /**
-     * Deleete an entry from DAO.
+     * Delete an entry from DAO.
      * @param id The deleted identifier.
+     * @returns The promise of the success of the operation.
      */
     delete(id: ID): Promise<boolean>;
 
 }
 
+/**
+ * An exception base class streamlining exception class generation.
+ * The exception handles the setting of the constructor name to the exception class name, 
+ * and wrapping the cause to the error.
+ * 
+ * An exceptoiin has an optional source value.
+ * 
+ * @todo Move this to a new file.
+ */
 export class Exception<TYPE = void, CAUSE = any> extends Error {
 
     /**
-     * The detail of the exception.
+     * The source value of the exception.
      */
     private _detail: TYPE | undefined;
 
-    constructor(msg: string, cause: CAUSE | undefined = undefined, detail: TYPE | undefined = undefined) {
+    /**
+     * Create a new exception.
+     * @param msg The error message.
+     * @param cause The cause of the exception.
+     * @param source The source value of the exception.
+     */
+    constructor(msg: string, cause: CAUSE | undefined = undefined, source: TYPE | undefined = undefined) {
         super(msg, { cause });
         this.name = this.constructor.name;
-        this._detail = detail;
+        this._detail = source;
     }
 
     /**
-     * The detail of the exception.
+     * The source value of the exception.
      */
-    get detail(): TYPE | undefined {
+    get source(): TYPE | undefined {
         return this._detail;
     }
 
@@ -188,18 +226,34 @@ export class Exception<TYPE = void, CAUSE = any> extends Error {
 /**
  * Exception indicating a resource was not found.
  */
-export class NotFoundException extends Exception {
+export class NotFoundException<ID> extends Exception<ID> {
 
-    constructor(msg: string = "Resoure not found") {
+    /**
+     * The default exception message for a not found exception.
+     */
+    static DefaultMessage = "Resource not found";
+
+    /**
+     * Create an exception indicating there was no content.
+     * @param msg The excpetion message. Defaults to the {@link #defaultMessage}
+     * @param id 
+     */
+    constructor(msg: string = NotFoundException.DefaultMessage, id : ID|undefined = undefined) {
         super(msg);
     }
 }
 
 /**
- * Exception indicating value was invalid.
+ * Exception indicating the DAO content was invalid.
  */
-export class InvalidValueException<TYPE, CAUSE = any> extends Exception<TYPE, CAUSE> {
+export class InvalidContentException<TYPE, CAUSE = any> extends Exception<TYPE, CAUSE> {
 
+    /**
+     * Create a new invalid content exception.
+     * @param msg The error message.
+     * @param detail The invalid content.
+     * @param cause The cause of the exception.
+     */
     constructor(msg: string = "Invalid resource value", detail: TYPE, cause: CAUSE | undefined = undefined) {
         super(msg, cause, detail);
     }
@@ -255,7 +309,7 @@ export default class MechDao implements Dao<Mech> {
                     return id;
                 });
             } else {
-                throw new InvalidValueException("Mech not found", content);
+                throw new InvalidContentException("Mech not found", content);
             }
         });
     }
@@ -267,14 +321,217 @@ export default class MechDao implements Dao<Mech> {
                     this.content.set(id, newContent);
                     return undefined;
                 } else {
-                    throw new InvalidValueException("Mech not found", newContent);
+                    throw new InvalidContentException("Mech not found", newContent);
                 }
             });
     }
     delete(id: string): Promise<boolean> {
         return Promise.resolve(this.content.delete(id));
     }
+}
 
+/**
+ * The postgresql query status information.
+ */
+export type Status = {
+    /**
+     * The completed command.
+     */
+    command: string,
+    /**
+     * The number of affected rows.
+     */
+    affectedRows: number,
+    /**
+     * The OID of the result.
+     * Implemented for backward compatibility.
+     * @deprecated
+     */
+    oid?: number
+}
+
+/**
+ * Parse status frmo pq status string.
+ * @param status The parsed status.
+ * @returns The status parsed from the string.
+ * @throws {SyntaxError} The status is not a valid status string.
+ */
+export function parseStatus(status: string | null): Status {
+    if (status == null) {
+        throw new SyntaxError("Status does not exist");
+    }
+    const match = /^(\w+)(?:\s+(\d+))?\s+(\d)\s*$/.exec(status);
+    if (match) {
+        if (match.length == 4) {
+            return {
+                command: match[1],
+                oid: Number(match[2]),
+                affectedRows: Number(match[3])
+            };
+
+        } else {
+            return {
+                command: match[1],
+                affectedRows: Number(match[2])
+            };
+        }
+    } else {
+        throw new SyntaxError("Not a valid status message");
+    }
+}
+
+/**
+ * A mech DAO implementation using Postgresql to store the resources.
+ */
+export class PqMechDao extends MechDao {
+
+    private connInfo: Configuration;
+
+    private _pqConnection: Client | undefined = undefined;;
+
+    /**
+     * Parse an identifier string.
+     * @param id The parsed identifier.
+     * @returns A safe integer or a bigint of the identifier.
+     * @throws {SyntaxError} The value was not a valid identifier. 
+     */
+    parseId(id: string): number|bigint {
+        if (/^0*[\da-fA-F]+$/.test(id)) {
+            if (id.length*4 > 52) {
+                // We need bigint.
+                return BigInt("0x" + id);
+            } else {
+                // we git a number
+                return Number.parseInt(id, 16);
+            }
+        } else {
+            throw new NotFoundException("An invalid identifier syntax");
+        }
+    }
+
+    /**
+     * Create a new PqMechDao.
+     * @param connection The ts-postges configuration for connection.
+     */
+    constructor(connection: Configuration | undefined = undefined) {
+        super();
+        if (connection != null) {
+            this.connInfo = connection;
+        } else {
+            this.connInfo = {
+                port: Number(process.env.DBPORT),
+                password: process.env.DBPASSWD,
+                user: process.env.DBUSER,
+                host: process.env.DBHOST ?? "localhost"
+            };
+        }
+        connect(this.connInfo).then(
+            (client) => {
+                this._pqConnection = client;
+            }
+        )
+    }
+
+    getAll(): Promise<[string, Mech][]> {
+        if (this._pqConnection != null) {
+            type ResultType = Mech & { id: number };
+            return this._pqConnection.query<ResultType>("SELECT id, model, name, weightClass, tonnage FROM Mech;").then(
+                (result) => {
+                    const array: Array<[string, Mech]> = result.rows.map((row) => {
+                        return [row.get("id").valueOf().toString(16), new Mech(row.get("model").toString(), row.get("name").toString(), "Humanoid", Number(row.get("tonnage").valueOf()))]
+                    });
+                    return array;
+                }
+            )
+        } else {
+            return Promise.resolve([]);
+        }
+    }
+
+    get(id: string): Promise<Mech> {
+        if (this._pqConnection != null) {
+            type ResultType = Mech;
+            return this._pqConnection.query<ResultType>("SELECT model, name, weightClass, tonnage FROM Mech WHERE id=$1;", [Number.parseInt(id, 16)]).then(
+                (result) => {
+                    if (result.rows.length == 1) {
+                        return new Mech(result.rows[0].get("model").toString(), result.rows[0].get("name").toString(), "Humanoid", Number(result.rows[0].get("tonnage").valueOf()));
+                    } else {
+                        throw new NotFoundException("No mech found", id);
+                    }
+                });
+        } else {
+            return Promise.reject(new NotFoundException("No connection to database"));
+        }
+
+    }
+
+    update(id: string, mech: Mech): Promise<void> {
+        if (this._pqConnection != null) {
+            type ResultType = Mech & { id: number };
+            return this._pqConnection.query<ResultType>("UPDATE Mech SET model=$1, name=$2, weightClass=$3, tonnage=$4 WHERE id=$5;",
+                [mech.model, mech.name, mech.weightClass, mech.tonnage, Number.parseInt(id, 16)]
+            ).then(
+                (result) => {
+                    const status = parseStatus(result.status);
+                    if (status.affectedRows == 0) {
+                        throw new NotFoundException("Cannot update a non-existing mech");
+                    } else {
+                        return undefined;
+                    }
+
+                },
+                (error) => {
+                    throw new InvalidContentException("The mech was invalid", mech);
+                }
+            );
+        } else {
+            return Promise.reject(new NotFoundException("No database available"));
+        }
+
+    }
+
+    delete(id: string): Promise<boolean> {
+        if (this._pqConnection != null) {
+            type ResultType = Mech & { id: number };
+            return this._pqConnection.query<ResultType>("DELETE FROM Mech WHERE id=$1;",
+                [Number.parseInt(id, 16)]
+            ).then(
+                (result) => {
+                    const status = parseStatus(result.status);
+                    return status.affectedRows == 1;
+                },
+                (error) => {
+                    throw error;
+                }
+            );
+        } else {
+            return Promise.reject(new NotFoundException("No database available"));
+        }
+
+    }
+
+    create(mech: Mech): Promise<string> {
+        if (this._pqConnection != null) {
+            type ResultType = Mech & { id: number };
+            return this._pqConnection.query<ResultType>("INSERT INTO Mech(model, name, weightClass, tonnage) VALUES ($1, $2, $3, $4, $5) RETURNING id;",
+                [mech.model, mech.name, mech.weightClass, mech.tonnage]
+            ).then(
+                (result) => {
+                    const status = parseStatus(result.status);
+                    if (status.affectedRows == 1) {
+                        return result.rows[0].get("id").toString(16);
+                    } else {
+                        throw new InvalidContentException("Creating a new mech failed", mech);
+                    }
+                },
+                (error) => {
+                    throw error;
+                }
+            );
+        } else {
+            return Promise.reject(new Error("No database available"));
+        }
+    }
 }
 
 const mechDb = new MechDao();
